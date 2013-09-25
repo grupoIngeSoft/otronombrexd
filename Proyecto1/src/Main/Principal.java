3b413e4066c50fab70b8d0c24edae87319f573fb
OtroUser <otro@otro>
2013-09-25 09:08:36 -0500

    Otro cambio desde repo2

--- Proyecto1/src/Main/Principal.java
+++ Proyecto1/src/Main/Principal.java
@@ -8,6 +8,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo genial");
 		System.out.println("Otro print XD");
+		System.out.println("Modificacion 3 kevininhe");
+		System.out.println("Modificacion 4 k999");
 
 	}
 
